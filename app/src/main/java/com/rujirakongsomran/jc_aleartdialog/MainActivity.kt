73e3d992af3a4272e4be6ebeee6544bc23947d1e
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-11-29 13:13:02 +0700

    Initial commit

---
+++ app/src/main/java/com/rujirakongsomran/jc_aleartdialog/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.rujirakongsomran.jc_aleartdialog
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.rujirakongsomran.jc_aleartdialog.ui.theme.JC_AleartDialogTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JC_AleartDialogTheme {
+
+            }
+        }
+    }
+}
